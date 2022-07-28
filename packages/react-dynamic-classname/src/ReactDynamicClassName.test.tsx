8f9acadd336a49a66dad4ecc692bb6b5a2d3ee71
dimaslz <me@dimaslz.io>
2022-07-28 19:38:22 +0200

    chore(test): update tests

--- packages/react-dynamic-classname/src/ReactDynamicClassName.test.tsx
+++ packages/react-dynamic-classname/src/ReactDynamicClassName.test.tsx
@@ -6,50 +6,205 @@ import ReactDynamicClassName from './ReactDynamicClassName';
 import { FC } from 'react';
 
 type PROPS = {
-	klass: string | object | string[] | object[]
+	children?: any | null,
+	klass?: string | object | string[] | object[]
 }
-const Demo: FC<PROPS> = ({ klass }: PROPS) => (
-	<ReactDynamicClassName>
-		<div d-className={klass}>
-      Hello World!
+
+const DummyComponent: FC<PROPS> = ({ klass }: PROPS) => (
+	<div d-classname={klass}>
+		<div d-classname={klass}>
+			<h3 d-classname={klass}>
+				Hello Dummy!
+			</h3>
 		</div>
-	</ReactDynamicClassName>
+	</div>
 );
 
+const Demo: FC<PROPS> = ({ klass, children = null }: PROPS) =>
+	 (<ReactDynamicClassName>
+		<h1 d-classname={klass}>
+			{children || 'Hello World!'}
+		</h1>
+	</ReactDynamicClassName>)
+;
+
 describe('ReactDynamicClassName', () => {
-	test('use correct class from an Array', () => {
-		const { getByText } = render(<Demo klass={[{ 'class-a': true }, { 'class-b': false }]} />);
-		const element = getByText('Hello World!');
-		expect(element).toBeInTheDocument();
-		expect(element.className).toBe('class-a');
-	});
-
-	describe('use correct class from an Object', () => {
-		test('class-a should be rendered', () => {
-			const { getByText } = render(<Demo klass={{ 'class-a': true, 'class-b': false }} />);
+	describe('Using html elements as children', () => {
+		test('use correct class from an Array', () => {
+			const { baseElement, getByText } = render(<Demo klass={[{ 'class-a': true }, { 'class-b': false }]} />);
 			const element = getByText('Hello World!');
 			expect(element).toBeInTheDocument();
-			expect(element.className).toBe('class-a');
+
+			expect(baseElement).toMatchSnapshot();
 		});
 
-		test('class-a and class-b should be rendered', () => {
-			const { getByText } = render(<Demo klass={{ 'class-a': true, 'class-b': true }} />);
+		test('use correct class from an Object', () => {
+			const { baseElement, getByText } = render(<Demo klass={{ 'class-a': true, 'class-b': false }} />);
 			const element = getByText('Hello World!');
 			expect(element).toBeInTheDocument();
-			expect(element.className).toBe('class-a class-b');
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('use correct class from an Object and Array', () => {
+			const { baseElement, getByText } = render(
+				<Demo
+					klass={[
+						{ 'class-a': true, 'class-b': false },
+						[{ 'class-c': true, 'class-d': false }],
+					]}
+				/>);
+			const element = getByText('Hello World!');
+			expect(element).toBeInTheDocument();
+
+			expect(baseElement).toMatchSnapshot();
 		});
 	});
 
-	test('use correct class from an Object and Array', () => {
-		const { getByText } = render(
-			<Demo
-				klass={[
+	describe('Using components as children', () => {
+		test('components as children with class as array of objects', () => {
+			const { baseElement, getByText } = render(<Demo>
+				<DummyComponent klass={[{ 'class-a': true }, { 'class-b': false }]} />
+			</Demo>);
+
+			const element = getByText('Hello Dummy!');
+			expect(element).toBeInTheDocument();
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('use correct class from an Object', () => {
+			const { baseElement, getByText } = render(<Demo>
+				<DummyComponent klass={{ 'class-a': true, 'class-b': false }} />
+			</Demo>);
+
+			const element = getByText('Hello Dummy!');
+			expect(element).toBeInTheDocument();
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('use correct class from an Object and Array', () => {
+			const { baseElement, getByText } = render(<Demo>
+				<DummyComponent klass={[
 					{ 'class-a': true, 'class-b': false },
 					[{ 'class-c': true, 'class-d': false }],
 				]}
-			/>);
-		const element = getByText('Hello World!');
-		expect(element).toBeInTheDocument();
-		expect(element.className).toBe('class-a class-c');
+				/>
+			</Demo>);
+
+			const element = getByText('Hello Dummy!');
+			expect(element).toBeInTheDocument();
+
+			expect(baseElement).toMatchSnapshot();
+		});
+	});
+
+	describe('Using multiple components as children', () => {
+		test('components as children with class as array of objects', () => {
+			const { baseElement, getAllByText } = render(<Demo>
+				<DummyComponent klass={[{ 'class-a': true }, { 'class-b': false }]} />
+				<DummyComponent klass={[{ 'class-a': true }, { 'class-b': false }]} />
+				<DummyComponent klass={[{ 'class-a': true }, { 'class-b': false }]} />
+			</Demo>);
+
+			const element = getAllByText('Hello Dummy!');
+			expect(element).toHaveLength(3);
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('use correct class from an Object', () => {
+			const { baseElement, getAllByText } = render(<Demo>
+				<DummyComponent klass={{ 'class-a': true, 'class-b': false }} />
+				<DummyComponent klass={{ 'class-a': true, 'class-b': false }} />
+				<DummyComponent klass={{ 'class-a': true, 'class-b': false }} />
+			</Demo>);
+
+			const element = getAllByText('Hello Dummy!');
+			expect(element).toHaveLength(3);
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('use correct class from an Object and Array', () => {
+			const { baseElement, getAllByText } = render(<Demo>
+				<DummyComponent klass={[
+					{ 'class-a': true, 'class-b': false },
+					[{ 'class-c': true, 'class-d': false }],
+				]}
+				/>
+				<DummyComponent klass={[
+					{ 'class-a': true, 'class-b': false },
+					[{ 'class-c': true, 'class-d': false }],
+				]}
+				/>
+				<DummyComponent klass={[
+					{ 'class-a': true, 'class-b': false },
+					[{ 'class-c': true, 'class-d': false }],
+				]}
+				/>
+			</Demo>);
+
+			const element = getAllByText('Hello Dummy!');
+			expect(element).toHaveLength(3);
+
+			expect(baseElement).toMatchSnapshot();
+		});
+	});
+
+	describe('Using multiple elements as children', () => {
+		test('elements as children with class as objects', () => {
+			const { baseElement } = render(<Demo>
+				<div d-classname={{ 'class-a': true, 'class-b': false }}>example A</div>
+				<div d-classname={{ 'class-a': true, 'class-b': false }}>example B</div>
+				<div d-classname={{ 'class-a': true, 'class-b': false }}>example C</div>
+			</Demo>);
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('elements as children with class as array of objects', () => {
+			const { baseElement } = render(<Demo>
+				<div d-classname={[{ 'class-a': true }, { 'class-b': false }]}>example A</div>
+				<div d-classname={[{ 'class-a': true }, { 'class-b': false }]}>example B</div>
+				<div d-classname={[{ 'class-a': true }, { 'class-b': false }]}>example C</div>
+			</Demo>);
+
+			expect(baseElement).toMatchSnapshot();
+		});
+
+		test('elements as children with class as Array and Object', () => {
+			const { baseElement } = render(<Demo>
+				<div d-classname={[
+					{ 'class-a': true, 'class-b': false },
+					[{ 'class-c': true, 'class-d': false }],
+				]}
+				>example A</div>
+				<div d-classname={[
+					{ 'class-a': true, 'class-b': false },
+					[{ 'class-c': true, 'class-d': false }],
+				]}
+				>example B</div>
+				<div d-classname={[
+					{ 'class-a': true, 'class-b': false },
+					[{ 'class-c': true, 'class-d': false }],
+				]}
+				>example C</div>
+			</Demo>);
+
+			expect(baseElement).toMatchSnapshot();
+		});
+	});
+
+	describe('Dummy', () => {
+		test('ClassName native', () => {
+			const { baseElement } = render(<ReactDynamicClassName>
+				<div className="class-a">example A</div>
+			</ReactDynamicClassName>);
+
+			console.log('baseElement', baseElement);
+			expect(baseElement).toMatchSnapshot();
+		});
 	});
 });
